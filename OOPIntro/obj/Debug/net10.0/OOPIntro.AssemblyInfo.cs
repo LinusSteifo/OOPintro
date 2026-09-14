2f019f48219f6e9c55caf3663bea30e63bdfd11a
LinusSteifo <linussteifo@gmail.com>
2026-09-14 10:56:02 +0200

    inheritance

--- OOPIntro/obj/Debug/net10.0/OOPIntro.AssemblyInfo.cs
+++ OOPIntro/obj/Debug/net10.0/OOPIntro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPIntro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c233297df5348b245ba5692657c73141ae05aa96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82f0a29ff78673a955f60a0b43f3e9e8fb90433")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPIntro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPIntro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
